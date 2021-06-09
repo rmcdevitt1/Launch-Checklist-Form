aa3f95bb2999841e070ed6d04f9ebcb64c0149b7
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-09 16:59:49 -0500

    began assignment

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // Write your JavaScript code here!
 
+
+let submit = document.getElementById("formSubmit");
+submit.addEventListener("click", function(event) {
+   event.preventDefault();
+   let pilotInput = document.querySelector("input[name=pilotName]");
+   
+
+}
+
+
+
+
+
+
+
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
