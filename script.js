cae4e37a18d6c30cb4592b32b32f7fdc1d719423
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-13 19:06:15 -0500

    added validation that isnt working lol

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 // Write your JavaScript code here!
 
 
-let submit = document.getElementById("formSubmit");
-submit.addEventListener("click", function(event) {
-   event.preventDefault();
-   let pilotInput = document.querySelector("input[name=pilotName]");
-   
+// let submit = document.getElementById("formSubmit");
+// submit.addEventListener("click", function(event) {
+//    event.preventDefault();
+//    let pilotInput = document.querySelector("input[name=pilotName]");
+//       if (pilotInput === "") {
+//          alert("good job!");
+//       }
+// }
 
-}
-
-
+window.addEventListener("load", function() {
+   let form = document.querySelector("launchForm");
+   form.addEventListener("formSubmit", function(event) {
+      event.preventDefault();
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      // alert the current value found in the username input
+      // alert("copilotName: " + copilotInput.value);
+      // alert("pilotName: " + pilotInput.value);
+      if (pilotInput.value === "" || copilotInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "" ) {
+         alert("All fields are required!");
+      }
+      if (isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
+         alert("Input must be a number!")
+      }
+   });
+});
 
 
 
