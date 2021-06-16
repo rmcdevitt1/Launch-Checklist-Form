40710161b7e01fd8cff9b306fbd36738b5c9aa80
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-16 16:44:25 -0500

    finished

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 // Write your JavaScript code here!
 
-
-// let submit = document.getElementById("formSubmit");
-// submit.addEventListener("click", function(event) {
-//    event.preventDefault();
-//    let pilotInput = document.querySelector("input[name=pilotName]");
-//       if (pilotInput === "") {
-//          alert("good job!");
-//       }
-// }
-
 window.addEventListener("load", function() {
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+        response.json().then(function(json) {
+            let div = document.getElementById("missionTarget");
+            div.innerHTML += `
+               <h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[5].name}</li>
+                  <li>Diameter: ${json[5].diameter}</li>
+                  <li>Star: ${json[5].star}</li>
+                  <li>Distance from Earth: ${json[5].distance}</li>
+                  <li>Number of Moons: ${json[5].moons}</li>
+               </ol>
+               <img src="${json[5].image}">
+               
+               `;
+
+        });
+    });
+
+/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+<h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${}</li>
+   <li>Diameter: ${}</li>
+   <li>Star: ${}</li>
+   <li>Distance from Earth: ${}</li>
+   <li>Number of Moons: ${}</li>
+</ol>
+<img src="${}">
+*/
+
+
+
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       event.preventDefault();
@@ -63,20 +87,3 @@ window.addEventListener("load", function() {
 });
 
 
-
-
-
-
-
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
